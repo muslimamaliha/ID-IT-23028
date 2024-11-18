60faea4737eafe4e22235214567ba071febad721
muslimamaliha <muslimamaliha32@gmail.com>
2024-11-18 21:04:07 +0600

    Add files via upload

---
+++ Ch1Ex1.java
@@ -0,0 +1,7 @@
+public class Ch1Ex1{
+ public static void main(String[] args){
+   System.out.println("Welcome to java");
+     System.out.println("Welcome to computer science");
+     System.out.println("Programming is fun");
+}
+}
